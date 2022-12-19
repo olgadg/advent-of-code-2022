733a632872e3d3ecbff4156fcee7950d42453978
Olga <olga@cottondroid.com>
2022-12-19 22:59:53 +0000

    Day 6

---
+++ app/day-6-2.rb
@@ -0,0 +1,21 @@
+class Day6
+
+  def readFile
+    input = File.readlines('day-6.input')
+    return input
+  end
+
+  def result
+    input = readFile[0]
+    (0..input.length - 14).each do |i|
+      marker = input[i, 14]
+      found = marker.chars.uniq.count { |char| marker.count(char) > 1 } == 0
+      if found
+        puts "found at " + (i + 14) .to_s  + " " + marker
+        break
+      end
+    end
+  end
+end
+
+Day6.new.result
